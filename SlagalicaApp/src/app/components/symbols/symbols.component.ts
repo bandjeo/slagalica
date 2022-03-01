76fea6472ab6de88ed425ef7c218400faa8c740c
bandjeo <branislav.andjelic@gmail.com>
2022-03-02 00:21:44 +0100

    symbols

--- SlagalicaApp/src/app/components/symbols/symbols.component.ts
+++ SlagalicaApp/src/app/components/symbols/symbols.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GamesService } from 'src/app/services/games.service';
 
 @Component({
   selector: 'app-symbols',
@@ -6,10 +8,116 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./symbols.component.css']
 })
 export class SymbolsComponent implements OnInit {
+  goal:any = []
 
-  constructor() { }
+  verticalArray = [0, 1, 2, 3, 4, 5]
+
+
+  comboHistory: any[] = [];
+  correctnessHistory: any[] = [];
+
+  currentCombo = [-1, -1, -1, -1];
+  ready = false;
+
+
+  currentSymbols = 0;
+  symbolMap:any = {
+    "0": 'nikola-vlahovic.png',
+    "1": 'tref.png',
+    "2": 'pik.png',
+    "3": 'herc.png',
+    "4": 'karo.png',
+    "5": 'star.jpeg'
+  }
+
+  done = false;
+
+  constructor(private games: GamesService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.goal = this.games.symbols[0].goal;
+  }
+
+  symbolClicked(symbol: number) {
+    for (let i in this.currentCombo) {
+      if (this.currentCombo[i] == -1) {
+        this.currentCombo[i] = symbol
+        break;
+      }
+    }
+    if (!this.currentCombo.includes(-1)) {
+      this.ready = true;
+    }
+  }
+
+  removeSymbol(index: number) {
+    this.currentCombo[index] = -1;
+    this.ready = false;
+  }
+
+  submitCombo() {
+    this.comboHistory.push([...this.currentCombo])
+    this.correctnessHistory.push(this.getCorrectnes())
+    if(this.compareCombos()) {
+      this.done = true;
+    }
+    if(this.comboHistory.length >= 6) {
+      this.done = true;
+    }
+    
+    this.currentCombo = [-1, -1, -1, -1]
+    this.ready = false;
+  }
+
+  compareCombos() {
+    if (this.currentCombo.length != this.goal.length) {
+      return false;
+    }
+
+    for (let i in this.currentCombo) {
+      if (this.currentCombo[i] != this.goal[i]) return false;
+    }
+    return true;
+  }
+
+  getCorrectnes() {
+    let combo = [...this.currentCombo]
+    let goal = [...this.goal]
+    let correctness = [];
+
+    for (let i in combo) {
+      if (combo[i] == goal[i]) {
+        correctness.push(true);
+        combo[i] = -1
+        goal[i] = -1
+      }
+    }
+    
+    for (let i in combo) {
+      if (combo[i] < 0) continue;
+      for (let j in goal) {
+        if(goal[j] < 0) continue
+        if (combo[i] == goal[j]) {
+          correctness.push(false)
+          combo[i] = -1
+          goal[j] = -1
+        }
+      }
+    }
+    return correctness
+  }
+
+  nextClicked() {
+    this.currentSymbols += 1;
+    if (this.currentSymbols + 1 > this.games.symbols.length) {
+      this.router.navigate(['questions'])
+    } else {
+      this.goal = this.games.symbols[this.currentSymbols].goal
+      this.comboHistory = [];
+      this.correctnessHistory = [];
+      this.currentCombo = [-1, -1, -1, -1]
+    }
   }
 
 }
